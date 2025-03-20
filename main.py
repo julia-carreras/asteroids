17bbebd1d619ae0a4044a671a4d952773ce2e11c
juliacarreras <julia.carreras03@gmail.com>
2025-03-20 11:10:30 +0100

    screen correction update and FPS implementation

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
-            pygame.Surface.fill(screen, (0, 0, 0), rect=None, special_flags=0)
+            screen.fill((0, 0, 0))
             pygame.display.flip()
+            
+            delta_time = clock.tick(60)
+            dt = delta_time / 1000
 
 
 
